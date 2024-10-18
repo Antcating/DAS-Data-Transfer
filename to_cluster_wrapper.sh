af86f283d21c7c793efaec7ca4a3a36deb722d9d
Antcating <yevhenii.fedorchenko@gmail.com>
2024-10-18 15:32:48 +0300

    fix: Fix logic error: status code reading from rsync

--- to_cluster_wrapper.sh
+++ to_cluster_wrapper.sh
@@ -1,8 +1,24 @@
-while [ $? -ne 0 ]; do
-	echo "Restarting..."
+pushd $(dirname $0) > /dev/null
+
+TELEGRAM_BOT_TOKEN=$(cat .logger/token)
+TELEGRAM_CHAT_ID=$(cat .logger/chat_id)
+
+attempt=1
+max_attempts=3
+/usr/bin/expect ./to_cluster.sh
+while [ $? -ne 0 ] && [ $attempt -lt $max_attempts ]; do
+	# curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Transfer has been restarted due to error (Attempt $attempt)"
+	echo "Restarting... (Attempt $attempt)"
 	sleep 30
-    	/usr/bin/expect {ABSOLUTE_PATH_TO_WORKING_DIR}/to_cluster.sh
-	curl -s -X POST https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id={TELEGRAM_CHAT_ID} -d text="Transfer has been restated due to error"
+	attempt=$((attempt + 1))
+	/usr/bin/expect ./to_cluster.sh
 done
-echo "Transfer finished successfully"
-curl -s -X POST https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id={TELEGRAM_CHAT_ID} -d text="Specified transder has been finished successfully"
+
+if [ $attempt -lt $max_attempts ]; then
+	echo "Transfer finished successfully"
+	curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Transfer finished successfully after $attempt attempts"
+else
+	error_log=$(tail -n 5 rsync.log)
+	echo "Transfer failed after $attempt attempts"
+	curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="Transfer failed after $attempt attempts:%0A%0ALog:%0A$error_log"
+fi
